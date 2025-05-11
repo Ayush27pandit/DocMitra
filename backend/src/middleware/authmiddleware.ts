df4f718f1ee977a66f4abd781ae0be1a8611c0d6
Ayush Pandit <ayush12rps@gmail.com>
2025-05-11 17:18:54 +0530

    bugs fixed

--- backend/src/middleware/authmiddleware.ts
+++ backend/src/middleware/authmiddleware.ts
@@ -11,30 +11,29 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import dotenv from 'dotenv';
 dotenv.config();
 
-const authMiddleware= (req: express.Request, res: express.Response, next: express.NextFunction) =>{
+export const authMiddleware= (req: express.Request, res: express.Response, next: express.NextFunction):void =>{
    
     try {
         const authHeader= req.headers.authorization;
-        if(!authHeader || !authHeader.startsWith("Bearer")){
-            return res.status(401).json({
+        if(!authHeader || !authHeader.startsWith("Bearer ")){
+             res.status(401).json({
                 message:"Invalid token"
             })
+            return
         }
         const token = authHeader.split(" ")[1];
-        const decode = jwt.verify(token, process.env.JWT_KEY as string);
+        const decode = jwt.verify(token, process.env.JWT_KEY as string) as JwtPayload;
         
-        req.userId = (decode as JwtPayload).userId;
+        req.userId = decode.userId;
         
         next();
 
     } catch (error) {
-        return res.status(401).json({
+        res.status(401).json({
             message:"Unauthorized"
         })  
+        return;
     }
    
 }
 
-module.exports = {
-  authMiddleware,
-};
